34795771f867b97783ef1500ef6292ed81c718a5
Vivek Bora <39134607+BoraVivek@users.noreply.github.com>
2022-08-23 07:57:10 +0530

    PatternPrint - Reverse Triangle

---
+++ PatternPrinting/ReverseTriangle.java
@@ -0,0 +1,37 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class ReverseTriangle {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+
+        int rows, cols;
+        System.out.print("Enter no. of rows:");
+        rows = s.nextInt();
+
+        System.out.print("Enter no. of Cols:");
+        cols = s.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (j < i) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+
+            /* Approach 2 */
+//            for(int j=0; j<i; j++){
+//                System.out.print("  ");
+//            }
+//
+//            for(int j=0; j<cols - i; j++){
+//                System.out.print("* ");
+//            }
+
+            System.out.println();
+        }
+    }
+}
